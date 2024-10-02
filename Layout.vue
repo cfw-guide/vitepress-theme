0d84599caec2a07b516463d9cb31ef2412cfb015
emiyl <nopanopey@gmail.com>
2024-10-02 23:53:59 +0100

    fix ad css

--- Layout.vue
+++ Layout.vue
@@ -9,10 +9,14 @@ const { Layout } = DefaultTheme
 <template>
     <Layout>
         <template #doc-before>
-            <div v-if="adUnits.length" :id="`waldo-tag-${adUnits[0]}`" style="padding-bottom: 2em;"></div>
+            <div v-if="adUnits.length" style="padding-bottom: 2em;">
+                <div :id="`waldo-tag-${adUnits[0]}`"></div>
+            </div>
         </template>
         <template #home-hero-after>
-            <div v-if="adUnits.length" :id="`waldo-tag-${adUnits[0]}`" style="margin-top: 3em;"></div>
+            <div v-if="adUnits.length" style="margin-top: 3em;">
+                <div :id="`waldo-tag-${adUnits[0]}`"></div>
+            </div>
         </template>
         <template #layout-bottom>
             <div v-if="adUnits.length" :id="`waldo-tag-${adUnits[1]}`"></div>
